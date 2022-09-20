9b8b0f749d2ede39431c4b998181c4e7d2a8d425
Mariano Jáuregui <marian_jauregui@hotmail.com>
2022-09-20 11:55:21 -0300

    Added Log4j configuration.

--- src/test/java/ExampleTest.java
+++ src/test/java/ExampleTest.java
@@ -1,9 +1,13 @@
+import io.restassured.http.ContentType;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.Test;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.*;
 
 public class ExampleTest {
+    private static final Logger LOGGER = LogManager.getLogger(ExampleTest.class);
 
     @Test
     public void requestZipCode(){
@@ -11,6 +15,9 @@ public class ExampleTest {
                 when().
                 get("http://zippopotam.us/us/90210").
                 then().assertThat()
-                .body("places[0].'place name'", equalTo("Beverly Hills"));
+                .body("places[0].'place name'", equalTo("Beverly Hills"))
+                .assertThat().statusCode(200)
+                .assertThat().contentType(ContentType.JSON);
     }
+
 }

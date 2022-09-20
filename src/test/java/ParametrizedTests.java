7f0f314af6e5495f2fb3c3b1aae558e53ef04f61
Mariano Jáuregui <marian_jauregui@hotmail.com>
2022-09-20 15:34:34 -0300

    Testing some more methods and adding requestSpecification

--- src/test/java/ParametrizedTests.java
+++ src/test/java/ParametrizedTests.java
@@ -1,4 +1,8 @@
 
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.specification.RequestSpecification;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
@@ -7,9 +11,15 @@ import static org.hamcrest.Matchers.equalTo;
 
 
 public class ParametrizedTests {
+    private static RequestSpecification requestSpecification;
+
+    @BeforeClass
+    public static void createRequest() {
+        requestSpecification = new RequestSpecBuilder().setBaseUri("http://api.zippopotam.us").build();
+    }
 
     @DataProvider(name = "Test 1")
-    public static Object[][] zipCodesAndPlaces(){
+    public static Object[][] zipCodesAndPlaces() {
         return new Object[][]{
                 { "us", "90210", "Beverly Hills"},
                 { "us", "12345", "Schenectady"},
@@ -26,9 +36,22 @@ public class ParametrizedTests {
                 get("http://zippopotam.us/{countryCode}/{zipCode}").
                 then().assertThat()
                 .body("places[0].'place name'", equalTo(expectedPlaceName));
-
                 //.assertThat().statusCode(200)
                 //.assertThat().contentType(ContentType.JSON)
                 //.log().body();
     }
+
+    @Test
+    public void requestZipCode(){
+        given().spec(requestSpecification).when().get("us/90210").then().assertThat().statusCode(200);
+    }
+
+    @Test
+    public void testExtractMethod(){
+        String placeName = given().spec(requestSpecification).when().get("http://zippopotam.us/us/90210")
+                .then().extract().path("places[0]. 'place name'");
+
+        Assert.assertEquals(placeName, "Beverly Hills");
+    }
+
 }

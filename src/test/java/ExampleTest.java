7c5487b0640a9f92269675b128651b01f5d03d13
Mariano Jáuregui <marian_jauregui@hotmail.com>
2022-09-20 14:14:45 -0300

    Adding log and running some other assertions to check the API

--- src/test/java/ExampleTest.java
+++ src/test/java/ExampleTest.java
@@ -9,15 +9,30 @@ import static org.hamcrest.Matchers.*;
 public class ExampleTest {
     private static final Logger LOGGER = LogManager.getLogger(ExampleTest.class);
 
+    private static final String URL = "http://zippopotam.us/us/90210";
     @Test
     public void requestZipCode(){
         given().
                 when().
-                get("http://zippopotam.us/us/90210").
+                get(URL).
                 then().assertThat()
-                .body("places[0].'place name'", equalTo("Beverly Hills"))
-                .assertThat().statusCode(200)
-                .assertThat().contentType(ContentType.JSON);
+                .body("places.'place name'", hasItem("Beverly Hills"))
+                //.assertThat().statusCode(200)
+                //.assertThat().contentType(ContentType.JSON)
+                .log().body();
     }
+
+    @Test
+    public void testState(){
+        given().when().get(URL).then().assertThat().body("places[0].state", equalTo("California"))
+                .and().assertThat().body("'post code'", not(hasItem("7777")));
+    }
+
+    @Test
+    public void testCountry(){
+        given().when().get(URL).then().assertThat().body("country", equalTo("United States"))
+                .and().assertThat().body("places[0].'state abbreviation'", equalTo("CA"));
+    }
+
 
 }
